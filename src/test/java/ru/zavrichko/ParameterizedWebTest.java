5ea020f8cc81d9a4ab8cc58e7d6afe77ff644ac2
a.zavrichko <andrey@zavrichko.dev>
2022-01-20 09:04:32 +0200

    add parametrized Test

--- src/test/java/ru/zavrichko/ParameterizedWebTest.java
+++ src/test/java/ru/zavrichko/ParameterizedWebTest.java
@@ -1,5 +1,31 @@
 package ru.zavrichko;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class ParameterizedWebTest {
+
+    @DisplayName("Тестирование общего алгоритма поиска")
+    @ValueSource(strings = {"Selenide", "Allure"})
+    @ParameterizedTest(name = "Тестирование общего алгоритма поиска с тестовыми данными: {0}")
+    void commonSearchTest(String testData) {
+        open("https://ya.ru");
+        $("#text").setValue(testData);
+        $("button[type='submit']").click();
+        $$("li.serp-item")
+                .first()
+                .shouldHave(text(testData));
+    }
+
+
+
+
+
+
+
 
 }
