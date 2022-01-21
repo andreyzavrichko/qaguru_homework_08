5064354158d29df08c410c1d72f88952b18b321d
a.zavrichko <andrey@zavrichko.dev>
2022-01-21 18:23:57 +0200

    add MethodSource

--- src/test/java/ru/zavrichko/ParameterizedWebTest.java
+++ src/test/java/ru/zavrichko/ParameterizedWebTest.java
@@ -2,8 +2,12 @@ package ru.zavrichko;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -30,6 +34,25 @@ public class ParameterizedWebTest {
     })
     @ParameterizedTest(name = "Тестирование общего алгоритма поиска Amazon с тестовыми данными: {0}")
     void csvSearchTest(String testData, String expectedResult) {
+        open("https://www.amazon.com/");
+        $("#twotabsearchtextbox").setValue(testData);
+        $("#nav-search-submit-button").click();
+        $("#search")
+                .shouldHave(text(expectedResult));
+    }
+
+    // method source
+    static Stream<Arguments> commonSearchTestDataProvider() {
+        return Stream.of(
+                Arguments.of("Car", "Cars"),
+                Arguments.of("Map", "The Map of Tiny Perfect Things")
+        );
+    }
+
+    @MethodSource("commonSearchTestDataProvider")
+    @DisplayName("Тестирование общего алгоритма поиска Amazon")
+    @ParameterizedTest(name = "Тестирование общего алгоритма поиска Amazon с тестовыми данными: {0}")
+    void methodSearchTest(String testData, String expectedResult) {
         open("https://www.amazon.com/");
         $("#twotabsearchtextbox").setValue(testData);
         $("#nav-search-submit-button").click();
