ff03edb21a07c406e18a0367523ead2f344da30e
a.zavrichko <andrey@zavrichko.dev>
2022-01-21 13:53:01 +0200

    add parametrized Test

--- src/test/java/ru/zavrichko/ParameterizedWebTest.java
+++ src/test/java/ru/zavrichko/ParameterizedWebTest.java
@@ -2,6 +2,7 @@ package ru.zavrichko;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.text;
@@ -9,20 +10,35 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class ParameterizedWebTest {
 
+    // value source
     @DisplayName("Тестирование общего алгоритма поиска")
-    @ValueSource(strings = {"Selenide", "Allure"})
+    @ValueSource(strings = {"car", "map"})
     @ParameterizedTest(name = "Тестирование общего алгоритма поиска с тестовыми данными: {0}")
     void commonSearchTest(String testData) {
-        open("https://ya.ru");
-        $("#text").setValue(testData);
-        $("button[type='submit']").click();
-        $$("li.serp-item")
-                .first()
+        open("https://www.amazon.com/");
+        $("#twotabsearchtextbox").setValue(testData);
+        $("#nav-search-submit-button").click();
+        $(".sg-col-inner")
                 .shouldHave(text(testData));
     }
 
 
 
+
+//    @DisplayName("Тестирование общего алгоритма поиска")
+//    @CsvSource(value = {
+//            "Selenide, Вышла Selenide ",
+//            "Junit, 5 is the next generation of "
+//    })
+//    @ParameterizedTest(name = "Тестирование общего алгоритма поиска с тестовыми данными: {0}")
+//    void csvSearchTest(String testData, String expectedResult) {
+//        open("https://ya.ru");
+//        $("#text").setValue(testData);
+//        $("button[type='submit']").click();
+//        $$("li.serp-item")
+//                .first()
+//                .shouldHave(text(expectedResult));
+//    }
 
 
 
